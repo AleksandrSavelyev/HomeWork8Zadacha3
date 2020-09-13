a2d3d8e51844d1edb412252516f8c4edd4f9aeb3
AleksandrSavelyev <punk558721973@gmail.com>
2020-09-14 00:23:21 +0300

    soxraneniye

---
+++ HomeWork8Zadacha3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWork8Zadacha3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle firstRect = new Rectangle();
+            Console.Write("введите длинну:\t");
+            firstRect.A = ChekVal();
+            Console.Write("введите ширину:\t");
+            firstRect.B = ChekVal();
+            firstRect.Info();
+            firstRect.Perimetr();
+            firstRect.Arrey();
+            if (firstRect.IsRectangle)
+                Console.WriteLine("заданный прямоугольник являеться квадратом");
+            else
+                Console.WriteLine("заданный прямоугольник не являеться квадратом");
+        }
+        static int ChekVal()
+        {
+            for (; ; )
+            {
+                if (int.TryParse(Console.ReadLine(), out int x))
+                    return x;
+                else
+                    Console.Write("не верные данные! повторите ввод:\t");
+            }
+        }
+    }
+}
---
+++ HomeWork8Zadacha3/Rectangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork8Zadacha3
+{
+    class Rectangle
+    {
+        private int a, b;
+        public Rectangle() { }
+        public Rectangle(int a, int b)
+        {
+        }
+
+        public int A
+        {
+            get { return a; }
+            set
+            {
+                if (value > 0)
+                    a = value;
+                else
+                    a = 1;
+            }
+
+        }
+        public int B
+        {
+            get { return b; }
+            set
+            {
+                if (value > 0)
+                    b = value;
+                else
+                    b = 1;
+            }
+
+        }
+        public bool IsRectangle
+        {
+            get
+            {
+                if (a == b)
+                    return true;
+                return false;
+            }
+        }
+        public void Info()
+        {
+            Console.WriteLine($"длинна прямоугольника равна {a} ширина равна {b}");
+        }
+        public void Perimetr()
+        {
+            int P = (a + b) * 2;
+            Console.WriteLine($"периметр прямоугольника равен {P}");
+        }
+        public void Arrey()
+        {
+            int S = a * b;
+            Console.WriteLine($"площадь прямоугольнгика равна {S}");
+        }
+    }
+}
